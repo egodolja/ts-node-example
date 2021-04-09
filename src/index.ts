dc35ecf28bef67f871800407860ff6e80e4f5761
elsigodolja <elsi@knockrentals.com>
2021-04-09 11:06:47 -0400

    revert type changes

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,14 @@ const main = () => {
     const name = 'Steve Jobs';
     const age = 35;
     const location = 'Palo Alto';
-    const hairColor = 'brown';
 
     const steve: Person ={
         name,
         age,
-        location,
-        hairColor
+        location
     };
 
-    console.log(`${steve.name} from ${steve.location} is ${steve.age} years old and has ${steve.hairColor} hair.`);
+    console.log(`${steve.name} from ${steve.location} is ${steve.age} years old.`);
 }
 
 main();
